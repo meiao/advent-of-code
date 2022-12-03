ce4f40a15585c9ddf23a41f3135788b9a2b902fa
Andre Onuki <otanuki@gmail.com>
2022-12-03 03:34:32 -0500

    2022 day 2

---
+++ ruby/2022/02/2p1.rb
@@ -0,0 +1,27 @@
+lines = File.open('2-input').readlines
+
+
+move_point = {
+  'A'.ord => 1,
+  'B'.ord => 2,
+  'C'.ord => 3
+}
+
+points = 0
+lines.each do |line|
+  this_round = 0
+  his, mine = line.split(' ').map{|c| c.ord}
+  mine = mine - 'X'.ord + 'A'.ord
+  this_round += move_point[mine]
+
+  if mine == his
+    this_round += 3
+  elsif mine - his == 1 || mine - his == -2
+    this_round += 6
+  end
+
+  points += this_round
+  p this_round
+end
+
+p points
---
+++ ruby/2022/02/2p2.rb
@@ -0,0 +1,39 @@
+lines = File.open('2-input').readlines
+
+
+round_point = {
+  'X'.ord => 0,
+  'Y'.ord => 3,
+  'Z'.ord => 6
+}
+
+resolution_add = {
+  'X'.ord => -1,
+  'Y'.ord => 0,
+  'Z'.ord => 1
+}
+
+move_point = {
+  ('A'.ord - 1) => 3,
+  'A'.ord => 1,
+  'B'.ord => 2,
+  'C'.ord => 3,
+  'D'.ord => 1
+}
+
+
+points = 0
+lines.each do |line|
+  this_round = 0
+  his, resolution = line.split(' ').map{|c| c.ord}
+
+
+  this_round += round_point[resolution]
+
+  this_round += move_point[his + resolution_add[resolution]]
+
+  points += this_round
+  p this_round
+end
+
+p points
