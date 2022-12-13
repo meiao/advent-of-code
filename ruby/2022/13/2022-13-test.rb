68bda006e65c06e2b5d41ed482c45d4fcdec163f
Andre Onuki <otanuki@gmail.com>
2022-12-13 00:36:27 -0500

    2022 day 13

---
+++ ruby/2022/13/2022-13-test.rb
@@ -0,0 +1,42 @@
+require 'minitest/autorun'
+require_relative '2022-13'
+
+# Tests for 2022 day 13
+class Test2022_13 < Minitest::Test
+
+  def test_compare
+    solver = Solver.new
+    assert_operator(solver.compare([1,1,3,1,1], [1,1,5,1,1]), :<=, -1)
+    assert_operator(solver.compare([[1],[2,3,4]], [[1],4]), :<=, -1)
+    assert_operator(solver.compare([9], [[8,7,6]]), :>=, 1)
+    assert_operator(solver.compare([[4,4],4,4], [[4,4],4,4,4]), :<=, -1)
+  end
+
+  def test_part1_small
+    file_name = File.join(File.dirname(__FILE__), './input.small')
+    input = File.open(file_name).readlines
+    solver = Solver.new
+    assert_equal(13, solver.solve(input))
+  end
+
+  def test_part1_large
+   file_name = File.join(File.dirname(__FILE__), './input')
+   input = File.open(file_name).readlines
+   solver = Solver.new
+   assert_equal(5393, solver.solve(input))
+  end
+
+  def test_part2_small
+   file_name = File.join(File.dirname(__FILE__), './input.small')
+   input = File.open(file_name).readlines
+   solver = Solver.new
+   assert_equal(140, solver.solve2(input))
+  end
+
+  def test_part2_large
+   file_name = File.join(File.dirname(__FILE__), './input')
+   input = File.open(file_name).readlines
+   solver = Solver.new
+   assert_equal(26712, solver.solve2(input))
+  end
+end
---
+++ ruby/2022/13/2022-13.rb
@@ -0,0 +1,43 @@
+#!/usr/local/bin/ruby
+
+# This program answers Advent of Code 2022 day 13
+#
+# This version ran in
+#
+# Author::    Andre Onuki
+# License::   GPL3
+class Solver
+  def solve(input)
+    index = 1
+    sum = 0
+    until input.empty?
+      left, right, _blank = input.shift(3)
+      sum += index if compare(eval(left.strip), eval(right.strip)) < 0
+      index += 1
+    end
+    sum
+  end
+
+  def solve2(input)
+    items = input.map{|line| line.strip}.filter{|line| !line.empty?}.map{|line| eval(line)}
+    items << [[2]]
+    items << [[6]]
+    items.sort!{|a,b| compare(a,b)}
+    (items.index([[2]])+1) * (items.index([[6]])+1)
+  end
+
+  def compare(left, right)
+    left_is_num = left.is_a?(Numeric)
+    right_is_num = right.is_a?(Numeric)
+    return left <=> right if left_is_num && right_is_num
+
+    return compare([left], right) if left_is_num
+    return compare(left, [right]) if right_is_num
+
+    [left.size, right.size].min.times do |i|
+      diff = compare(left[i], right[i])
+      return diff if diff != 0
+    end
+    return left.size - right.size
+  end
+end
