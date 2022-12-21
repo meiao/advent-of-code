edf8ca3986cf4c8ac98a9cb681eeb7467a7961d0
Andre Onuki <otanuki@gmail.com>
2022-12-21 09:21:54 -0500

    2022 day 21

---
+++ ruby/2022/21/2022-21-test.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require_relative '2022-21'
+
+# Tests for 2022 day 21
+class Test2022_21 < Minitest::Test
+  # def test_part1_small
+  #   file_name = File.join(File.dirname(__FILE__), './input.small')
+  #   input = File.open(file_name).readlines
+  #   solver = Solver.new(input)
+  #   assert_equal(152, solver.solve)
+  # end
+  #
+  # def test_part1_large
+  #  file_name = File.join(File.dirname(__FILE__), './input')
+  #  input = File.open(file_name).readlines
+  #  solver = Solver.new(input)
+  #  assert_equal(152479825094094, solver.solve)
+  # end
+
+  # def test_part2_small
+  #  file_name = File.join(File.dirname(__FILE__), './input.small')
+  #  input = File.open(file_name).readlines
+  #  solver = Solver.new(input)
+  #  assert_equal(1, solver.solve2)
+  # end
+
+  def test_part2_large
+   file_name = File.join(File.dirname(__FILE__), './input')
+   input = File.open(file_name).readlines
+   solver = Solver.new(input)
+   assert_equal(3360561285172, solver.solve2)
+  end
+end
---
+++ ruby/2022/21/2022-21.rb
@@ -0,0 +1,110 @@
+#!/usr/local/bin/ruby
+
+# This program answers Advent of Code 2022 day 21
+#
+# This version ran in
+#
+# Author::    Andre Onuki
+# License::   GPL3
+class Solver
+
+  def initialize(input)
+    @monkeys = {}
+    input.each do |line|
+      name, expr = line.strip.split(': ')
+      expr = expr.to_i if expr.index(/\d+/) == 0
+      @monkeys[name] = expr
+    end
+  end
+
+  def solve
+    calculate('root')
+  end
+
+  def calculate(name)
+    return @monkeys[name] if @monkeys[name].is_a?(Numeric)
+    name1, op, name2 = @monkeys[name].split(' ')
+    value1 = calculate(name1)
+    value2 = calculate(name2)
+    @monkeys[name] = apply(value1, op, value2)
+  end
+
+  def apply(v1, op, v2)
+    case op
+    when '+'
+      return v1 + v2
+    when '-'
+      return v1 - v2
+    when '*'
+      return v1 * v2
+    when '/'
+      return v1 / v2
+    end
+  end
+
+  def solve2
+    @monkeys['humn'] = '(humn)'
+    name1, _, name2 = @monkeys['root'].split(' ')
+
+    value1 = calculate2(name1)
+    value2 = calculate2(name2)
+    p value1
+    p value2
+    if value1.is_a?(Numeric)
+      return decalculate(value1, value2)
+    else
+      return decalculate(value2, value1)
+    end
+  end
+
+  def calculate2(name)
+    return @monkeys[name] if @monkeys[name].is_a?(Numeric)
+    return '(humn)' if name == 'humn'
+    name1, op, name2 = @monkeys[name].split(' ')
+    value1 = calculate2(name1)
+    value2 = calculate2(name2)
+    if value1.is_a?(Numeric) && value2.is_a?(Numeric)
+      return @monkeys[name] = apply(value1, op, value2)
+    end
+    return @monkeys[name] = "(#{value1} #{op} #{value2})"
+  end
+
+  def decalculate(number, humn)
+    return number if humn == '(humn)'
+    match = humn.match(/^\((\(.*\)) ([+\-*\/]) (\d+)\)$/)
+    if match != nil
+      return decalculate(deapply(number, match[2], match[3].to_i), match[1])
+    end
+    match = humn.match(/^\((\d+) ([+\-*\/]) (\(.*\))\)$/)
+    if match != nil
+      return decalculate(reverse_apply(number, match[2], match[1].to_i), match[3])
+    end
+    throw Error("One regex shoulda matched")
+  end
+
+  def deapply(v1, op, v2)
+    case op
+    when '+'
+      return v1 - v2
+    when '-'
+      return v1 + v2
+    when '*'
+      return v1 / v2
+    when '/'
+      return v1 * v2
+    end
+  end
+
+  def reverse_apply(v1, op, v2)
+    case op
+    when '+'
+      return v1 - v2
+    when '-'
+      return (v1 - v2) * -1
+    when '*'
+      return v1 / v2
+    when '/'
+      return v2 / v1
+    end
+  end
+end
