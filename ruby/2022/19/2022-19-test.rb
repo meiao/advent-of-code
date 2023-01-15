30b5612551f5ec14a3cc75af94ecc7ccc3a4aaab
Andre Onuki <otanuki@gmail.com>
2023-01-14 22:38:42 -0500

    2022 day 19

--- ruby/2022/19/2022-19-test.rb
+++ ruby/2022/19/2022-19-test.rb
@@ -7,27 +7,27 @@ class Test2022_19 < Minitest::Test
     file_name = File.join(File.dirname(__FILE__), './input.small')
     input = File.open(file_name).readlines
     solver = Solver.new
-    assert_equal(33, solver.solve(input, 24))
+    assert_equal(33, solver.solve(input, 24, 2))
   end
 
-  #def test_part1_large
-  #  file_name = File.join(File.dirname(__FILE__), './input')
-  #  input = File.open(file_name).readlines
-  #  solver = Solver.new
-  #  assert_equal(1, solver.solve(input))
-  #end
+  def test_part1_large
+   file_name = File.join(File.dirname(__FILE__), './input')
+   input = File.open(file_name).readlines
+   solver = Solver.new
+   assert_equal(1177, solver.solve(input, 24, 30))
+  end
 
-  #def test_part2_small
+  # def test_part2_small
   #  file_name = File.join(File.dirname(__FILE__), './input.small')
   #  input = File.open(file_name).readlines
   #  solver = Solver.new
-  #  assert_equal(1, solver.solve2(input))
-  #end
+  #  assert_equal(1, solver.solve2(input, 32, 2))
+  # end
 
-  #def test_part2_large
-  #  file_name = File.join(File.dirname(__FILE__), './input')
-  #  input = File.open(file_name).readlines
-  #  solver = Solver.new
-  #  assert_equal(1, solver.solve2(input))
-  #end
+  def test_part2_large
+   file_name = File.join(File.dirname(__FILE__), './input')
+   input = File.open(file_name).readlines
+   solver = Solver.new
+   assert_equal(62744, solver.solve2(input, 32, 3))
+  end
 end
--- ruby/2022/19/2022-19.rb
+++ ruby/2022/19/2022-19.rb
@@ -1,7 +1,9 @@
 #!/usr/local/bin/ruby
 
 # This program answers Advent of Code 2022 day 19
-#
+# I cheated and looked for some ideas in reddit. The ideas that helped me were:
+# 1. whenever you wait and you could have built a type of robot, do not create that type of robot in the next minute
+# 2. do not create more of one type of robot than the max cost of that resource. ie, if the recipe that uses the max ore uses 5 ores, do not have more than 5 ore robots.
 # This version ran in
 #
 # Author::    Andre Onuki
@@ -13,11 +15,18 @@ end
 
 class Solver
   def solve(input, time)
-    input.map{|line| Blueprint.new(line).calculate(Status.initial(time))}.sum
+    status = Status.initial(time)
+    input.map{|line| Blueprint.new(line)}.map{|blueprint| blueprint.id * blueprint.calculate([status], time)}.sum
+  end
+
+  def solve2(input, time, lines)
+    status = Status.initial(time)
+    input[0..(lines - 1)].map{|line| Blueprint.new(line).calculate([status], time)}.reduce(1){|x,y| x*y}
   end
 end
 
 class Blueprint
+  attr_reader :id
   def initialize(line)
     tokens = line.split(' ')
     @id = tokens[1].to_i
@@ -27,58 +36,75 @@ class Blueprint
       :obsidian => {:ore => tokens[18].to_i, :clay => tokens[21].to_i, :obsidian => 0},
       :geode => {:ore => tokens[27].to_i, :clay => 0, :obsidian => tokens[30].to_i}
     }.freeze
-    @max = 0
+    @max_costs = {}
+    Resources::LIST.each do |r|
+      @max_costs[r] = @costs.values.map{|costs| costs[r]}.max
+    end
   end
 
-  def calculate(status)
-    recurse(status)
-    @max * @id
+  def calculate(statuses, time)
+    cur_statuses = statuses
+    time.times do |i|
+      p i
+      cur_statuses = cur_statuses.map{|status| status.next_statuses(@costs, @max_costs)}.flatten
+      cur_statuses = clean_statuses(cur_statuses)
+    end
+    max = cur_statuses.map{|status| status.resources[:geode]}.max
+    max
   end
 
-  def recurse(status)
-    if status.time == 0
-      if status.score > @max
-        @max = status.score
-        p @max
+  def clean_statuses(statuses)
+    if statuses.size > 10000
+      p statuses.size
+      return statuses
+    end
+    cur_statuses = statuses.sort{|x, y| y.value <=> x.value}
+    next_statuses = []
+    until cur_statuses.empty?
+      status = cur_statuses.pop
+      if cur_statuses.none?{|s| status.is_worse_than?(s)}
+        next_statuses << status
       end
-      return
     end
-    if status.score + (status.time*(status.time - 1)/2) < @max
-      p "discarded with time #{status.time}"
-      return
-    end
-    status.next_statuses(@costs).each{|st| recurse(st)}
+    p "#{statuses.size} -> #{next_statuses.size}"
+    next_statuses
   end
+
 end
 
 class Status
-  attr_reader :time, :resources, :bots
-  def initialize(resources, bots, time)
+  attr_reader :time, :resources, :bots, :could_have_built
+  def initialize(resources, bots, could_have_built, time)
     @resources = resources
     @bots = bots
     @time = time
+    @could_have_built = could_have_built
   end
 
-  def next_statuses(costs)
+  def next_statuses(costs, max_costs)
     next_resources = {}
     Resources::LIST.each do |resource|
       next_resources[resource] = @resources[resource] + @bots[resource]
     end
     statuses = []
-    base_status = Status.new(next_resources, @bots, time - 1)
-    statuses.concat(buy_statuses(base_status, costs))
+    could_have_built = Resources::LIST.filter { |resource| costs[resource].all?{|k, v| @resources[k] >= v} }
+    base_status = Status.new(next_resources, @bots, could_have_built, time - 1)
+    statuses.concat(buy_statuses(base_status, costs, max_costs))
     statuses << base_status
   end
 
-  def buy_statuses(status, costs)
+  def buy_statuses(status, costs, max_costs)
     next_statuses = []
-    Resources::LIST.each do |resource|
+    resources = Resources::LIST
+    resources -= @could_have_built
+    resources.each do |resource|
+      next if @bots[resource] == max_costs[resource]
       if costs[resource].all?{|k, v| @resources[k] >= v}
         next_resources = Hash[status.resources]
         costs[resource].each {|k, v| next_resources[k] -= v}
         next_bots = Hash[status.bots]
         next_bots[resource] += 1
-        next_statuses << Status.new(next_resources, next_bots, status.time)
+        next_statuses << Status.new(next_resources, next_bots, [], status.time)
       end
     end
     next_statuses
@@ -86,6 +112,14 @@ class Status
 
   def score
     @resources[:geode]
+  end
+
+  def value
+    [@bots.values.sum, @resources.values.sum]
+  end
+
+  def is_worse_than?(status)
+    Resources::LIST.all?{|r| @bots[r] <= status.bots[r] && @resources[r] <= status.resources[r]}
   end
 
   def self.initial(time)
@@ -101,7 +135,7 @@ class Status
       :obsidian => 0,
       :geode => 0
     }
-    Status.new(initial_resources, initial_bots, time)
+    Status.new(initial_resources, initial_bots, [], time)
   end
 end
 
