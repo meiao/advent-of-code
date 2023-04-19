c513c075613559f34f2e62497d1c7441de124194
Andre Onuki <otanuki@gmail.com>
2023-04-18 20:29:24 -0400

    2022 day 22

---
+++ ruby/2022/22/2022-22-2.rb
@@ -0,0 +1,60 @@
+#!/usr/local/bin/ruby
+
+# This program answers Advent of Code 2022 day 22
+#
+# This version ran in
+#
+# Author::    Andre Onuki
+# License::   GPL3
+
+require_relative '2022-22'
+
+class Solver2 < Solver
+  def solve(input)
+    map = create_map(input[0..-3])
+    me = Me.new(map)
+    cmds = input[-1].strip.chars
+    until cmds.empty?
+      num = ''
+      while cmds[0] != nil && cmds[0] != 'R' && cmds[0] != 'L'
+        num << cmds.shift
+      end
+      me.move(num.to_i)
+      dir = cmds.shift
+      me.turn(dir) if dir != nil
+    end
+    coord = me.cur_cell.point
+    1000 * (coord[1] + 1) + 4 * (coord[0] + 1) + me.cur_dir.value
+  end
+
+  def create_map(lines)
+    map = {}
+    lines.each_with_index do |line, y|
+      line.rstrip.chars.each_with_index do |c, x|
+        next if c == ' '
+        map[[x, y]] = (c == '.' ? Cell.new([x,y], map) : Wall.instance)
+      end
+    end
+    create_special_moves(map)
+    map
+  end
+
+  def create_special_moves(map)
+    50.times do |i|
+      map[[149, i]].set_next_cell(:R, map[[99, 149 - i]], :L)
+      map[[99, 50 + i]].set_next_cell(:R, map[[100 + i, 49]], :U)
+      map[[99, 100 + i]].set_next_cell(:R, map[[149, 49 - i]], :L)
+      map[[49, 150 + i]].set_next_cell(:R, map[[50 + i, 149]], :U)
+      map[[50, i]].set_next_cell(:L, map[[0, 149 - i]], :R)
+      map[[50, 50 + i]].set_next_cell(:L, map[[i, 100]], :D)
+      map[[0, 100 + i]].set_next_cell(:L, map[[50, 49 - i]], :R)
+      map[[0, 150 + i]].set_next_cell(:L, map[[50 + i, 0]], :D)
+      map[[i, 199]].set_next_cell(:D, map[[100 + i, 0]], :D)
+      map[[50 + i, 149]].set_next_cell(:D, map[[49, 150 + i]], :L)
+      map[[100 + i, 49]].set_next_cell(:D, map[[99, 50 + i]], :L)
+      map[[i, 100]].set_next_cell(:U, map[[50, 50 + i]], :R)
+      map[[50 + i, 0]].set_next_cell(:U, map[[0, 150 + i]], :R)
+      map[[100 + i, 0]].set_next_cell(:U, map[[i, 199]], :U)
+    end
+  end
+end
--- ruby/2022/22/2022-22-test.rb
+++ ruby/2022/22/2022-22-test.rb
@@ -1,14 +1,15 @@
 require 'minitest/autorun'
 require_relative '2022-22'
+require_relative '2022-22-2'
 
 # Tests for 2022 day 22
 class Test2022_22 < Minitest::Test
-  # def test_part1_small
-  #   file_name = File.join(File.dirname(__FILE__), './input.small')
-  #   input = File.open(file_name).readlines
-  #   solver = Solver.new
-  #   assert_equal(6032, solver.solve(input))
-  # end
+  def test_part1_small
+    file_name = File.join(File.dirname(__FILE__), './input.small')
+    input = File.open(file_name).readlines
+    solver = Solver.new
+    assert_equal(6032, solver.solve(input))
+  end
 
   def test_part1_large
    file_name = File.join(File.dirname(__FILE__), './input')
@@ -17,6 +18,7 @@ class Test2022_22 < Minitest::Test
    assert_equal(103224, solver.solve(input))
   end
 
+  # part 2 small is currently unsolvable given the hardcoded stuff in the code
   #def test_part2_small
   #  file_name = File.join(File.dirname(__FILE__), './input.small')
   #  input = File.open(file_name).readlines
@@ -24,10 +26,10 @@ class Test2022_22 < Minitest::Test
   #  assert_equal(1, solver.solve2(input))
   #end
 
-  #def test_part2_large
-  #  file_name = File.join(File.dirname(__FILE__), './input')
-  #  input = File.open(file_name).readlines
-  #  solver = Solver.new
-  #  assert_equal(1, solver.solve2(input))
-  #end
+  def test_part2_large
+   file_name = File.join(File.dirname(__FILE__), './input')
+   input = File.open(file_name).readlines
+   solver = Solver2.new
+   assert_equal(189097, solver.solve(input))
+  end
 end
