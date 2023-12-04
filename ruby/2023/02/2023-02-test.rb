73989a1369c92ac44c0b5fad035fa05d67dbbb84
Andre Onuki <otanuki@gmail.com>
2023-12-04 08:51:10 -0500

    2023 day 2

---
+++ ruby/2023/02/2023-02-test.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require_relative '2023-02'
+
+# Tests for 2023 day 02
+class Test2023_02 < Minitest::Test
+  # def test_part1_small
+  #   file_name = File.join(File.dirname(__FILE__), './input.small')
+  #   input = File.open(file_name).readlines
+  #   solver = Solver.new
+  #   assert_equal(8, solver.solve(input))
+  # end
+  #
+  # def test_part1_large
+  #  file_name = File.join(File.dirname(__FILE__), './input')
+  #  input = File.open(file_name).readlines
+  #  solver = Solver.new
+  #  assert_equal(2449, solver.solve(input))
+  # end
+
+  def test_part2_small
+   file_name = File.join(File.dirname(__FILE__), './input.small')
+   input = File.open(file_name).readlines
+   solver = Solver.new
+   assert_equal(2286, solver.solve2(input))
+  end
+
+  def test_part2_large
+   file_name = File.join(File.dirname(__FILE__), './input')
+   input = File.open(file_name).readlines
+   solver = Solver.new
+   assert_equal(63981, solver.solve2(input))
+  end
+end
---
+++ ruby/2023/02/2023-02.rb
@@ -0,0 +1,52 @@
+#!/usr/local/bin/ruby
+
+# This program answers Advent of Code 2023 day 02
+#
+# Author::    Andre Onuki
+# License::   GPL3
+class Solver
+  def solve(input)
+    ball_count = {
+      'red'   => 12,
+      'green' => 13,
+      'blue'  => 14
+    }
+    sum = 0
+    input.each do |line|
+      possible = true
+      game, data = line.split(':')
+      sets = data.split('; ')
+      sets.each do |set|
+        balls = set.split(', ')
+        balls.each do |count_color|
+          count, color = count_color.split(' ')
+          if count.to_i > ball_count[color]
+            possible = false
+            break
+          end
+        end
+        break if !possible
+      end
+      sum += game.split(' ')[1].to_i if possible
+    end
+    sum
+  end
+
+  def solve2(input)
+    sum = 0
+    input.each do |line|
+      min_balls = Hash.new(0)
+      game, data = line.split(':')
+      sets = data.split('; ')
+      sets.each do |set|
+        balls = set.split(', ')
+        balls.each do |count_color|
+          count, color = count_color.split(' ')
+          min_balls[color] = count.to_i if count.to_i > min_balls[color]
+        end
+      end
+      sum += min_balls['red'] * min_balls['green'] * min_balls['blue']
+    end
+    sum
+  end
+end
