c0579d5487f1083870b8bf97e1b602b71eeb1abe
Andre Onuki <otanuki@gmail.com>
2024-01-04 20:48:08 -0500

    2023 day 17 uses a simplified dijkstra but part 2 still took 30 minutes

---
+++ ruby/2023/17/2023-17-test.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require_relative '2023-17'
+
+# Tests for 2023 day 17
+class Test2023_17 < Minitest::Test
+  def test_part1_small
+    file_name = File.join(File.dirname(__FILE__), './input.small')
+    input = File.open(file_name).readlines
+    solver = Solver.new
+    assert_equal(102, solver.solve(input))
+  end
+
+  def test_part1_large
+   file_name = File.join(File.dirname(__FILE__), './input')
+   input = File.open(file_name).readlines
+   solver = Solver.new
+   assert_equal(963, solver.solve(input))
+  end
+
+  def test_part2_small
+   file_name = File.join(File.dirname(__FILE__), './input.small')
+   input = File.open(file_name).readlines
+   solver = Solver.new
+   assert_equal(94, solver.solve2(input))
+  end
+
+  def test_part2_large
+   file_name = File.join(File.dirname(__FILE__), './input')
+   input = File.open(file_name).readlines
+   solver = Solver.new
+   assert_equal(1178, solver.solve2(input))
+  end
+end
