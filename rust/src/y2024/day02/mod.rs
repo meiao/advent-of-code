a1f160e22c53c5b3c4860c32061cd7d57f9ec4a8
Andre Onuki <otanuki@gmail.com>
2024-12-02 18:18:23 -0500

    2024 day 2 Rust

---
+++ rust/src/y2024/day02/mod.rs
@@ -0,0 +1,50 @@
+use std::collections::HashMap;
+
+fn part1(input: Vec<&str>) -> usize {
+    input.iter()
+        .map(convert_line)
+        .filter(safe)
+        .count()
+}
+
+fn convert_line(line: &&str) -> Vec<i64> {
+    line.split_whitespace().map(&str::parse::<i64>)
+        .map(Result::unwrap)
+        .collect()
+}
+
+fn safe(levels: &Vec<i64>) -> bool {
+    let increasing = &levels[0] < &levels[1];
+    for i in 0..(levels.len() -1) {
+        let diff = levels[i] - levels[i + 1];
+        let still_increasing = levels[i] < levels[i+1];
+        if diff == 0 || diff.abs() > 3 || (increasing != still_increasing) {
+            return false
+        }
+    }
+    true
+}
+
+fn part2(input: Vec<&str>) -> usize {
+    input.iter()
+        .map(convert_line)
+        .filter(safe2)
+        .count()
+}
+
+fn safe2(levels: &Vec<i64>) -> bool {
+    if safe(levels) {
+        return true;
+    }
+    for i in 0..(levels.len()) {
+        let mut copy = levels.clone();
+        copy.remove(i);
+        if safe(&copy) {
+            return true;
+        }
+    }
+    false
+}
+
+#[cfg(test)]
+mod test;
