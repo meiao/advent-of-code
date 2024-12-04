2cb35df42223de7f0c74b4cfda985193fe900ce5
Andre Onuki <otanuki@gmail.com>
2024-12-03 19:45:17 -0500

    2024 day 3 Ruby DRYing the code

--- ruby/2024/03/2024-03.rb
+++ ruby/2024/03/2024-03.rb
@@ -6,30 +6,38 @@
 # License::   GPL3
 class Solver
   def solve(input)
-    re = /mul\((\d{1,3}),(\d{1,3})\)/
-    input.map { |line| line.scan(re) }
-         .flatten(1)
-         .map { |m| m[0].to_i * m[1].to_i }
-         .sum
+    re = /(mul\(\d{1,3},\d{1,3}\))/
+    cmds = process_input(input, re)
+    execute(cmds)
   end
 
   def solve2(input)
     re = /(do\(\))|(mul\(\d{1,3},\d{1,3}\))|(don't\(\))/
-    sum = 0
-    enabled = true
-    input.map { |line| line.scan(re) }
-         .flatten
-         .compact
-         .each do |cmd|
-      if cmd == 'do()'
-        enabled = true
-      elsif cmd == 'don\'t()'
-        enabled = false
-      elsif enabled
-        nums = cmd[4..-2].split(',').map(&:to_i)
-        sum += nums[0] * nums[1]
-      end
-    end
-    sum
+    cmds = process_input(input, re)
+    execute(cmds)
   end
+end
+
+def process_input(input, re)
+  input.map { |line| line.scan(re) }
+       .flatten
+       .compact
+end
+
+# cmds is a list of strings that contains valid cmds
+# do(), don't() or mul(x,y)
+def execute(cmds)
+  sum = 0
+  enabled = true
+  cmds.each do |cmd|
+    if cmd == 'do()'
+      enabled = true
+    elsif cmd == 'don\'t()'
+      enabled = false
+    elsif enabled
+      nums = cmd[4..-2].split(',').map(&:to_i)
+      sum += nums[0] * nums[1]
+    end
+  end
+  sum
 end
