6c9d8ce2acd74d5d407d83c5a3fca176063c1e30
Andre Onuki <otanuki@gmail.com>
2025-12-01 08:50:29 -0500

    2025 day 1

---
+++ ruby/2025/01/2025-01-test.rb
@@ -0,0 +1,39 @@
+require 'minitest/autorun'
+require_relative '2025-01'
+
+# Tests for 2025 day 01
+class Test2025_01 < Minitest::Test
+
+  def test_rot
+    dial = Dial.new
+    assert_equal(50, dial.num)
+    dial.rotate('R1')
+    assert_equal(51, dial.num)
+    dial.rotate('L52')
+    assert_equal(99, dial.num)
+  end
+
+  def test_passed_0
+    solver = Solver.new
+    assert_equal(1, solver.passed_0(1, 'L1'))
+    assert_equal(2, solver.passed_0(1, 'L101'))
+    assert_equal(0, solver.passed_0(82, 'L68'))
+    assert_equal(1, solver.passed_0(99, 'R1'))
+    assert_equal(2, solver.passed_0(99, 'R101'))
+    assert_equal(3, solver.passed_0(99, 'R203'))
+  end
+
+  def test_part1_small
+    file_name = File.join(File.dirname(__FILE__), './input')
+    input = File.open(file_name).readlines.map(&:strip)
+    solver = Solver.new
+    assert_equal(3, solver.solve(input))
+  end
+
+  def test_part2
+   file_name = File.join(File.dirname(__FILE__), './input')
+   input = File.open(file_name).readlines.map(&:strip)
+   solver = Solver.new
+   assert_equal(6, solver.solve2(input))
+  end
+end
---
+++ ruby/2025/01/2025-01.rb
@@ -0,0 +1,56 @@
+#!/usr/local/bin/ruby
+
+# This program answers Advent of Code 2025 day 01
+#
+# Author::    Andre Onuki
+# License::   GPL3
+class Solver
+  def solve(input)
+    dial = Dial.new
+    zeroes = 0
+    input.each do |cmd|
+      dial.rotate(cmd)
+      zeroes += 1 if dial.num == 0
+    end
+    zeroes
+  end
+
+  def solve2(input)
+    dial = Dial.new
+    prev = dial.num
+    zeroes = 0
+    input.each do |cmd|
+      zeroes += passed_0(prev, cmd)
+      prev = dial.rotate(cmd)
+    end
+    zeroes
+  end
+
+  def passed_0(prev, cmd)
+    value = cmd[1..].to_i
+    if cmd[0] == 'R' || prev == 0
+      (prev + value) / 100
+    else
+      (100 - prev + value) / 100
+    end
+  end
+end
+
+class Dial
+  attr_reader :num
+
+  def initialize
+    @num = 50
+  end
+
+  def rotate(cmd)
+    value = cmd[1..].to_i
+    if cmd[0] == 'R'
+      @num += value
+    else
+      @num -= value
+    end
+    @num = @num %100
+    @num
+  end
+end
