3da68cc62f97b3e3e9754529da9c824e36dbc78d
André Onuki <otanuki@gmail.com>
2025-12-04 10:23:11 -0500

    Update 2025-04.rb
    Cleaning up code

--- ruby/2025/2025-04.rb
+++ ruby/2025/2025-04.rb
@@ -5,12 +5,7 @@ class Solver
 
 
   def solve(input, repeat)
-    map = Hash.new(false)
-    input.length.times do |y|
-      input[y].length.times do |x|
-        map[[x,y]] = true if input[y][x] == '@'
-      end
-    end
+    map = create_map(input)
 
     initial_rolls = map.size
 
@@ -18,29 +13,39 @@ class Solver
     keys_to_check = map.keys
 
     loop do
-      removable_keys = []
+      removable_keys = keys_to_check.select{|key| can_remove?(map, key)}
 
-      keys_to_check.each do |key|
-        neighboring = @@neighbors.map{|dir| [key[0] + dir[0], key[1] + dir[1]]}
-                                 .select{|pos| map[pos]}
-                                 .size
-        removable_keys << key if neighboring < 4
-      end
-      removable_keys.each{|key|map.delete(key)}
-
+      removable_keys.each {|key|map.delete(key)}
       break unless repeat && !removable_keys.empty?
+
       # for the next iteration, only rows neighboring a removed roll will be checked
       keys_to_check = affected_keys(map, removable_keys)
     end
     initial_rolls - map.size
   end
 
+  def create_map(input)
+    map = Hash.new(false)
+    input.length.times do |y|
+      input[y].length.times do |x|
+        map[[x,y]] = true if input[y][x] == '@'
+      end
+    end
+    map
+  end
+
+  def can_remove?(map, key)
+    neighboring = @@neighbors.map{|dir| [key[0] + dir[0], key[1] + dir[1]]}
+                             .select{|pos| map[pos]}
+    neighboring.size < 4
+  end
+
   def affected_keys(map, removed_keys)
     affected_keys = Hash.new(false)
     removed_keys.each do |key|
-      @@neighbors.map{|dir| [key[0] + dir[0], key[1] + dir[1]]}
-                 .select{|pos| map[pos]}
-                 .each{|pos| affected_keys[pos] = true}
+      @@neighbors.map{|dir| [key[0] + dir[0], key[1] + dir[1]]} # neighbors of a removed roll
+                 .select{|pos| map[pos]}                        # keep only positions that currently have a roll
+                 .each{|pos| affected_keys[pos] = true}         # save to "set"
     end
     affected_keys.keys
   end
