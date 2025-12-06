6293789d6f8788f58ccbd14c9260d3c40269a9fc
Andre Onuki <otanuki@gmail.com>
2025-12-06 17:31:07 -0500

    2025 day 6

---
+++ ruby/2025/06/2025-06-test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require_relative '2025-06'
+
+# Tests for 2025 day 06
+class Test2025_06 < Minitest::Test
+  def test_part1
+    file_name = File.join(File.dirname(__FILE__), './input')
+    input = File.open(file_name).readlines.map(&:strip)
+    solver = Solver.new
+    assert_equal(4277556, solver.solve(input))
+  end
+
+  def test_part2
+   file_name = File.join(File.dirname(__FILE__), './input')
+   input = File.open(file_name).readlines
+   solver = Solver.new
+   assert_equal(3263827, solver.solve2(input))
+  end
+end
---
+++ ruby/2025/06/2025-06.rb
@@ -0,0 +1,50 @@
+#!/usr/local/bin/ruby
+
+# This program answers Advent of Code 2025 day 06
+#
+# Author::    Andre Onuki
+# License::   GPL3
+
+class Solver
+  def solve(input)
+    ops = input[-1].split(' ')
+    data = input[0..-2].map{|line| line.split(' ').map(&:to_i)}
+    problems = []
+    (ops.size - 1).downto(0).each do |i|
+      op = ops[i]
+      numbers = data.map{|line| line[i]}
+      problems << [op, numbers]
+    end
+    problems.map{|problem| calc(problem)}
+            .sum
+  end
+
+  def calc(problem)
+    op = problem[0]
+    op == '+' ?
+      problem[1].sum :
+      problem[1].inject(:*)
+  end
+
+  def solve2(input)
+    ops = input[-1]
+    problems = []
+    numbers = []
+    (ops.size).downto(0).each do |i|
+      op = ops[i]
+      col = column(i, input)
+      numbers << col.to_i unless col.strip.empty?
+      if op == '+' || op == '*'
+        problems << [op, numbers]
+        numbers = []
+      end
+    end
+    problems.map{|problem| calc(problem)}
+            .sum
+  end
+
+  def column(column, input)
+    input.map{|line| line[column]}
+         .join
+  end
+end
