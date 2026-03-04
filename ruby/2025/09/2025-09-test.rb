ee6d37ae5f6e9f9a931f1ac38ef8b874f5e00042
Andre Onuki <otanuki@gmail.com>
2026-03-03 22:34:49 -0500

    2025 day 09 Changed the implementation to check if a rectangle is inside the area. Also fixed a bug in the area calculation.

--- ruby/2025/09/2025-09-test.rb
+++ ruby/2025/09/2025-09-test.rb
@@ -11,9 +11,18 @@ class Test2025_09 < Minitest::Test
   end
 
   def test_part2
-   file_name = File.join(File.dirname(__FILE__), './input')
-   input = File.open(file_name).readlines.map(&:strip)
-   solver = Solver.new
-   assert_equal(24, solver.solve(input, false))
+    file_name = File.join(File.dirname(__FILE__), './input')
+    input = File.open(file_name).readlines.map(&:strip)
+    solver = Solver.new
+    assert_equal(24, solver.solve(input, false))
+  end
+
+  def test_cross
+    solver = Solver.new
+    assert_equal(false, solver.cross?([[0, 0], [2, 0]], [[0, 1], [2, 1]]))
+    assert_equal(false, solver.cross?([[2, 0], [2, 2]], [[3, 1], [3, 4]]))
+    assert_equal(false, solver.cross?([[2, 0], [2, 2]], [[10, 1], [15, 1]]))
+    assert_equal(false, solver.cross?([[10, 1], [15, 1]], [[2, 0], [2, 2]]))
+    assert_equal(true, solver.cross?([[10, 1], [15, 1]], [[12, 0], [12, 3]]))
   end
 end
